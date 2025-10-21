61a514b580f2749e89be60f0a2139c9922a580c4
maxi <maximiliano.besoain@gmail.com>
2025-10-21 14:36:33 -0300

    chore: Change @vitejs/react-plugin-swc to @vitejs/react-plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), visualizer()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('@mui/material')) {
-                            return 'mui';
-                        }
-                        if (id.includes('@uiw/react-codemirror')) {
-                            return 'codemirror';
-                        }
-                    }
-                }
+  plugins: [react(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui/material')) {
+              return 'mui';
             }
+            if (id.includes('@uiw/react-codemirror')) {
+              return 'codemirror';
+            }
+          }
         }
+      }
     }
+  }
 });
